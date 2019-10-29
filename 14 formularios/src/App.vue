671764eafc27d1418236735f82442343a4f8d880
Frederick <G7.alexander@gmail.com>
2019-10-29 16:45:02 -0400

    Continuando bloque de formularios

--- 14 formularios/src/App.vue
+++ 14 formularios/src/App.vue
@@ -1,60 +1,184 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-6">
+        <h1>Formularios</h1>
+        <form action>
+          <div class="form-group">
+            <label for="nombre">Nombre</label>
+            <input
+              type="text"
+              name="nombre"
+              id
+              class="form-control"
+              placeholder="Nombre"
+              v-model.trim="usuario.nombre"
+            />
+          </div>
+          <div class="form-group">
+            <label for="correo">Correo Electronico</label>
+            <input
+              type="email"
+              name="correo"
+              id
+              class="form-control"
+              placeholder="Correo Electronico"
+              v-model.lazy="usuario.correo"
+            />
+          </div>
+          <div class="form-group">
+            <label for="clave">Contraseña</label>
+            <input
+              type="password"
+              name="clave"
+              id
+              class="form-control"
+              placeholder="Contraseña"
+              v-model="usuario.clave"
+            />
+          </div>
+          <div class="form-group">
+            <label for="edad">Edad</label>
+            <input
+              type="text"
+              name="edad"
+              id
+              class="form-control"
+              placeholder="Edad"
+              v-model.number="usuario.edad"
+            />
+          </div>
+          <div class="form-group">
+            <label for="pais">Pais</label>
+            <select class="form-control" v-model="usuario.pais">
+              <option v-for="pais in paises">{{pais}}</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="mensaje">Mensaje</label>
+            <textarea class="form-control" rows="3" name="mensaje" v-model="usuario.mensaje"></textarea>
+          </div>
+          <fieldset class="form-group">
+            <legend>Genero</legend>
+            <div class="form-check">
+              <div class="form-check-label">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  name="optionRadios"
+                  value="hombre"
+                  checked
+                  v-model="usuario.genero"
+                />
+                Hombre
+              </div>
+            </div>
+            <div class="form-check">
+              <div class="form-check-label">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  name="optionRadios"
+                  value="mujer"
+                  v-model="usuario.genero"
+                />
+                Mujer
+              </div>
+            </div>
+          </fieldset>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                v-model="usuario.condiciones"
+                value="acepto"
+              />
+              Acepto las condiciones
+            </label>
+          </div>
+          <div class="form-check">
+            <label class="form-check-label">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                v-model="usuario.condiciones"
+                value="newslatter"
+              />
+              Acepto la Newslatter
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary" @click.prevent="enviarFormulario()">Enviar</button>
+        </form>
+      </div>
+      <div class="resultados col-6" v-if="mostrar">
+        <h1>Resultados:</h1>
+        <p>
+          <strong>Nombre:</strong>
+          {{usuario.nombre}} - {{usuario.nombre.length}}
+        </p>
+        <p>
+          <strong>Correo Electronico:</strong>
+          {{usuario.correo}}
+        </p>
+        <p>
+          <strong>Contraseña:</strong>
+          {{usuario.clave}}
+        </p>
+        <p>
+          <strong>Edad:</strong>
+          {{usuario.edad}} - {{typeof usuario.edad}}
+        </p>
+        <p>
+          <strong>Pais:</strong>
+          {{usuario.pais}}
+        </p>
+        <p class="mensaje">
+          <strong>Mensaje:</strong>
+          {{usuario.mensaje}}
+        </p>
+        <p>
+          <strong>Genero:</strong>
+          {{usuario.genero}}
+        </p>
+        <p>
+          <strong>Condiciones:</strong>
+          {{usuario.condiciones}}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      usuario: {
+        nombre: "Frederick",
+        correo: "deeply@deeply.com",
+        clave: "miclave",
+        edad: 24,
+        pais: "Venezuela",
+        mensaje: "Este es el mensaje",
+        genero: "hombre",
+        condiciones: []
+      },
+      mostrar: false,
+      paises: ["Venezuela", "Argentina", "Panama"]
+    };
+  },
+  methods: {
+    enviarFormulario() {
+      this.mostrar = true;
+      console.log("Hiciste click en el boton");
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.mensaje {
+  white-space: pre;
 }
 </style>
